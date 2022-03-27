f12e85b7311be8baf6e7585fd078a5a19adb6878
stuart-rickard <stuart@bau-dev.com>
2022-03-27 16:22:37 -0700

    git logo

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,7 +2,23 @@ import React, { useState } from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <>
+      <h1>
+        🦶{" "}
+        <span>
+          <a href="https://github.com/stuart-rickard">
+            <img src="../assets/git-logo-lt.png"></img>
+            <img src="../assets/git-logo.png"></img>
+          </a>
+        </span>
+        <span>
+          <a href="https://www.linkedin.com/in/stuart-rickard-594a044/">in</a>{" "}
+        </span>
+        <span></span> 🦶
+      </h1>
+    </>
+  );
 }
 
 export default Footer;
